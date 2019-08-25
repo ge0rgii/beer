780acb2bb90564250d6a3e4f6cdc2587956d9e84
Georgii Stanishevskii <jerzy_staniszewski@yahoo.com>
2019-08-25 14:47:30 +0200

    Updates

---
+++ precache-manifest.e5edec89ff54f019650eebddbdf17b5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641ce2160cbe18abfc56955aec002ff2",
+    "url": "/beer/index.html"
+  },
+  {
+    "revision": "6e5df2b84de5f3847b56",
+    "url": "/beer/static/css/2.d4b275d9.chunk.css"
+  },
+  {
+    "revision": "beb264c376f37118681b",
+    "url": "/beer/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6e5df2b84de5f3847b56",
+    "url": "/beer/static/js/2.ba586768.chunk.js"
+  },
+  {
+    "revision": "beb264c376f37118681b",
+    "url": "/beer/static/js/main.0ec2d615.chunk.js"
+  },
+  {
+    "revision": "27f96a4fcffd1c7d6266",
+    "url": "/beer/static/js/runtime~main.79763047.js"
+  }
+]);
